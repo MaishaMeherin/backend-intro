52105b49fc0cf99cf8bd8ab13b66a9b56821196b
MaishaMeherin <maisha.meherin1307@gmail.com>
2025-05-31 01:12:55 +0600

    create and test todo

--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -12,7 +12,14 @@ router.get('/', (req, res) => {
 
 
 //create todo
-router.post('/', (req, res) => {})
+router.post('/', (req, res) => {
+    const { task } = req.body
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)    
+
+    insertTodo.run(req.userId, task)
+
+    res.json({ id: insertTodo.lastID, task , completed: 0 })
+})
 
 //update a todo
 router.put('/:id', (req, res) => {
