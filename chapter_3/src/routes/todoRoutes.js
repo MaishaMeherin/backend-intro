0a121d8ff6e572d24632753362317add6e6ad209
MaishaMeherin <maisha.meherin1307@gmail.com>
2025-05-31 01:39:34 +0600

    Update todo and test

--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -16,14 +16,21 @@ router.post('/', (req, res) => {
     const { task } = req.body
     const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)    
 
-    insertTodo.run(req.userId, task)
+    const result = insertTodo.run(req.userId, task)
 
-    res.json({ id: insertTodo.lastID, task , completed: 0 })
+    res.json({ id: result.lastInsertRowid, task , completed: 0 })
 })
 
 //update a todo
 router.put('/:id', (req, res) => {
+    const { completed } = req.body
+    const { id } = req.params
+    const { page } = req.query
 
+    const updatedTodo = db.prepare('UPDATE todos SET completed = ? WHERE id = ?')
+
+    updatedTodo.run(completed, id)
+    res.json( {message: 'Todo Completed'} )
 }) 
 
 //delete a todo
